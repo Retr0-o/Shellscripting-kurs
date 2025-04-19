2933cb0263fece0c1844b2025c95dcb0c109e67e
Retr0-o <hugobacke@hotmail.se>
2025-04-19 23:30:02 +0200

    Arbete lektion 6

---
+++ Lektion6/LogMonitor.ps1
@@ -0,0 +1,47 @@
+# LogMonitor.ps1 - Övervakar loggfiler för misstänkta nyckelord.
+# Skapat av: Hugo Backe, April 2025.
+# Syfte: Skanna loggar, logga resultat och varna om problem.
+
+# _______ Variabler _______
+$LogDir = "C:\Users\Hugo\Documents\GitHub\Shellscripting-kurs\Lektion6\Logs"                    # Mapp för loggar som ska analyseras.
+$OutputLog = "C:\Users\Hugo\Documents\GitHub\Shellscripting-kurs\Lektion6\Logs\monitor.log"     # Fil där info om loggarna ska skrivas till.
+$Keywords = @("error","failed")         # Nyckelord för filanalyseringen.
+
+# _______ Funktioner _______
+function Write-Log {        # Funktion för att skriva info om loggar till en fil. 
+    param (
+        [Parameter(Mandatory=$true)][string]$Level,         # Lokal variabel för att sätta en nivå på logg meddelandet. "[string]" säkerställer att de är text.
+        [Parameter(Mandatory=$true)][string]$Message        # Valbart meddelande för att väljas och användas när man ska köra denna funktion.
+    )
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"     # Variabel för att plocka tid och datum. "-Format" gör så att man kan välja tidsformatet själv.
+    "$timestamp [$Level] $Message" | Out-File -FilePath $OutputLog -Append        # Funktionens huvudlogik. Tar variablerna och sätter ihop dom till ett meddelande som sedan skrivs till $OutputLog filen.
+}                                                                                 # "-Append" gör så att ny text läggs till på redan existerande text, inte byter ut.
+
+# _______ Validering _______
+Write-Log -Level "INFO" -Message "Startar loggövervakning..."   # Användning av funktionen "Write-Log" och inmatning av parametrar.
+if (-not(Test-Path $LogDir)) {       # Testar så att sökvägen för $LogDir är 
+    Write-Log -Level "ERROR" -Message "Katalogen $LogDir finns inte."
+    Write-Error "Katalogen finns inte"
+}
+
+# _______ Huvudlogik _______        # Huvudlogiken i detta skript. För varje loggfil så kollar den igenom efter specifika nyckelord. 
+try {
+    $files = Get-ChildItem -Path $LogDir -File -ErrorAction Stop
+    foreach ($file in $files) {
+        $content = Get-Content $file.FullName -ErrorAction Stop
+        foreach ($line in $content) {
+            foreach ($keyword in $Keywords) {
+                if ($line -match $keyword) {
+                    $Message = "Hittade '$keyword' i $($file.Name): $line"
+                    Write-Log -Level "WARNING" -Message $Message
+                    Write-Warning $Message
+                }
+            }
+        }
+    }
+    Write-Log -Level "INFO" -Message "Skanning klar."
+} catch {
+    Write-Log -Level "INFO" -Message "Fel: $($.Exception.Message)"
+    Write-Error "Fel vid skanning: $($.Exception.Message)"
+    exit 1
+}
